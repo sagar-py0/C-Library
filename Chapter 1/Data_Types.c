79b132ea234d23bd97201eb3710e5d6782349689
sagar-py0 <133792723+sagar-py0@users.noreply.github.com>
2025-11-24 14:16:47 +0530

    Data_Types.c

---
+++ Chapter 1/Data_Types.c
@@ -0,0 +1,11 @@
+#inlcude <stdio.h>
+  int main(){
+    int a=1;
+    float b=0.11;
+    double c=0.6969;
+    char d='x';
+    printf("The value of a is : %d",a);
+    printf("The value of b is : %f",b);
+    printf("The value of c is : %f",c);
+    printf("The value of d is : %c",d);
+  }
