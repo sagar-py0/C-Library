9938524046e0bba082184a244fbd95cd6f3b80e7
sagar-py0 <133792723+sagar-py0@users.noreply.github.com>
2025-11-24 14:32:18 +0530

    Add comments to 03_Comments.c
    Added comments to clarify code purpose and logic.

---
+++ Chapter 1/03_Comments.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+  int main(){
+  printf("Hello");
+  // This is known as comment.
+  /* This is a multi line comment.
+      They help clarify the purpose and logic of the code, making it easier to understand, debug, and modify*/
+  }
