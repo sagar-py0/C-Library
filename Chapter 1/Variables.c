38a142b35baaf52cda266f8dae9d11ddddb85a3a
sagar-py0 <133792723+sagar-py0@users.noreply.github.com>
2025-11-24 14:07:40 +0530

    Variables.c
    This file contains a simple C program that initializes an integer variable and prints its value. It also includes comments on variable naming conventions.

---
+++ Chapter 1/Variables.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main(){
+    int a;
+    a = 1;
+    printf("%d",a);
+   return 0;
+}
+
+
+// 1.No commas, blanks are allowed.
+// 2.Only underscore is allowed (_) from special characters.
+// 3.First letter must be lower case alpabet or underscore(_).
