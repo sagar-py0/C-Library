c5df5ab2bb8399d171e6f29dc3b942b9501e0476
sagar-py0 <133792723+sagar-py0@users.noreply.github.com>
2025-11-24 14:26:55 +0530

    Fix include typo and update variable values

--- Chapter 1/02_Data_Types.c
+++ Chapter 1/02_Data_Types.c
@@ -1,11 +1,11 @@
-#inlcude <stdio.h>
+#include <stdio.h>
   int main(){
-    int a=1;
-    float b=0.11;
+    int a=69;
+    float b=0.69;
     double c=0.6969;
     char d='x';
-    printf("The value of a is : %d",a);
-    printf("The value of b is : %f",b);
-    printf("The value of c is : %f",c);
-    printf("The value of d is : %c",d);
+    printf("The value of a is : %d\n",a);
+    printf("The value of b is : %f\n",b);
+    printf("The value of c is : %f\n",c);
+    printf("The value of d is : %c\n",d);
   }
